dfb00a2e14b74c8bd2f08c40bed62e82f14f42cf
byjavex <byjavex@gmail.com>
2025-10-03 11:59:29 +0200

    Proyecto de prueba csi

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Prueba</title>
+</head>
+<body>
+   <h1>Hola Mundo</h1> 
+   <p>wenas tardes</p>
+
+
+   <a href="" class="btn">
+    <button>Click </button>
+   </a> dawdada
+
+    awdawdad
+</body>
+</html>
